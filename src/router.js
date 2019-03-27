e2b6e162f698d8e5a195a8e54c0cd8abb5f04f1a
铁腕阿童木 <zhengzehao@maka.im>
2019-03-27 17:57:24 +0800

    test

--- src/router.js
+++ src/router.js
@@ -4,6 +4,10 @@ import Home from './views/Home.vue'
 
 Vue.use(Router)
 
+function loadView(view) {
+  return () => import(/* webpackChunkName: "view-[request]" */ `@/views/${view}.vue`)
+}
+
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
@@ -11,7 +15,8 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      // component: Home
+      component: loadView('Home')
     },
     {
       path: '/about',
@@ -20,6 +25,14 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/aaa',
+      name: 'aaa',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "are_u_ok" */ './views/aaa.vue')
     }
   ]
 })
---
+++ src/views/aaa.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <img :src="imgUrl" alt="" @click='changeImg'>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            imgArr :[require('../assets/001.jpg'),require('../assets/002.jpeg'),require('../assets/003.jpg'),require('../assets/004.jpg'),require('../assets/005.jpeg'),require('../assets/006.png')],
+            imgUrl:''
+        }
+    },
+    created(){
+        this.imgUrl = this.imgArr[0]
+    },
+    methods:{
+        changeImg(){
+            let idx = parseInt(Math.random()*10)%(this.imgArr.length)
+            this.imgUrl  = this.imgArr[idx]
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+img 
+    width 200px
+    cursor pointer
+</style>
+
+
